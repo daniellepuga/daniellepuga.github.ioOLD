0c44dc2080bf95f3ae7280738426885bbe6e9565
Danielle Puga <danielle.puga@gmail.com>
2019-10-09 09:09:45 -0700

    Continuation

---
+++ cs261-dynamic-array-master/dynamic_array.py
@@ -0,0 +1,27 @@
+# DynamicArray: An array that grows to accommodate new elements.
+# Your implementation should pass the tests in test_dynamic_array.py.
+# YOUR NAME
+
+class DynamicArray:
+    capacity = 10
+
+    def __init__(self):
+        self.capacity = 10
+
+    def is_empty(self):
+        return True
+
+    def __len__(self):
+        return 0
+
+    def append(self, int):
+        pass
+    
+    def __getitem__(self, index):
+        return 42
+
+    def append(self, index):
+        return 8
+
+    # def darray(self, type):
+    #     return type
---
+++ hash-table.py
@@ -0,0 +1,12 @@
+mydict ={
+    "Apple": 20,
+    "Banana": 5,
+    "Cherry": 72,
+    "Donut": 19,
+    "Elderberry": 111,
+    "Fig": 86,
+    "Grape": 5,
+    "Honeydew Melon": 13,
+    "Ice Cream": 245,
+}
+print(mydict.get("Grape"))
